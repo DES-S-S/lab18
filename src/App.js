7286a3416cb2519e1b9317686025d9ff036a180c
DES-S-S <gavriluko24@gmail,com>
2026-05-18 11:31:58 +0300

    lab199

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
 
   return (
-    <Router>
+    <Router basename="/lab18">
       <div className="app-container">
         <Header toggleTheme={toggleTheme} theme={theme} />
         <main className="content">
